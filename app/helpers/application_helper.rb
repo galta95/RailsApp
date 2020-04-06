1323d109b9fcd4166f86ee71aa9278789beb6d83
galta95 <galtaebi@gmail.com>
2020-04-06 20:59:29 +0300

    Updated styles for home page, nav bar and footer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
-    def login_helper
+    def login_helper (style)
         if current_user.is_a?(GuestUser)
-            (link_to "Registration", new_user_registration_path, method: :get) +
-            "<br>".html_safe +
-            (link_to "Login", new_user_session_path, method: :get)
+            (link_to "Registration", new_user_registration_path, method: :get, class: style) +
+            " ".html_safe +
+            (link_to "Login", new_user_session_path, method: :get, class: style)
         else
-            link_to "Logout", destroy_user_session_path, method: :delete
+            link_to "Logout", destroy_user_session_path, method: :delete, class: style
         end
     end
 
@@ -14,5 +14,12 @@ module ApplicationHelper
             greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout"
             content_tag(:p, greeting, class: "source-greeting")
         end
+    end
+
+    def copyright_generator
+        time = Time.new
+        authour = "Gal Taebi."
+        greeting = "\u00A9 #{time.year} | #{authour} All rights reserved"
+        content_tag(:p, greeting)
     end
 end
