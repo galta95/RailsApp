ce2b56d4677713fef65bceb2a46a3a51909cbfd3
galta95 <galtaebi@gmail.com>
2020-06-11 12:46:40 +0300

    Implemented partials for navigation, footer and masthead for the portfolio layout

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,7 +59,6 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  config.reload_classes_only_on_change = false
   
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
